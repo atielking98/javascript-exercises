32ea2b3681b1eb5dfbc442ae2cdb99a79870a2ca
Allison Tielking <atielkin@alumni.stanford.edu>
2022-07-25 14:58:44 +0200

    solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || typeof start != "number" || typeof end != "number") {
+        return "ERROR";
+    }
+    if (end < start) {
+        let savedVal = end;
+        end = start;
+        start = savedVal;
+    }
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
