73eaf2b97f9c30c9af4422082ef9e6c1bcd087a9
Allison Tielking <atielkin@alumni.stanford.edu>
2022-07-25 13:13:57 +0200

    solve repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, numRepetitions) {
+    if (numRepetitions < 0) {
+        return "ERROR";
+    }
+    let initVal = 0;
+    let finalStr = "";
+    while (initVal < numRepetitions) {
+        finalStr += text;
+        initVal++;
+    }
+    return finalStr;
 };
 
 // Do not edit below this line
