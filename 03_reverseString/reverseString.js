1c305992e5ba14161e7cc2d7548c6dbb5aa75c71
Allison Tielking <atielkin@alumni.stanford.edu>
2022-07-25 13:17:05 +0200

    solve reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(originalString) {
+    let originalStringArr = originalString.split("");
+    let newStr = "";
+    for (let i = originalStringArr.length - 1; i >= 0; i--) {
+        newStr += originalStringArr[i];
+    }
+    return newStr;
 };
 
 // Do not edit below this line
