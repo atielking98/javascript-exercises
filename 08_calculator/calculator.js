bf8e16d95696733f72ea38128a3ac16f1c70ca93
Allison Tielking <atielkin@alumni.stanford.edu>
2022-07-27 14:53:13 +0200

    solve calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  arr.forEach(item => sum += item);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  arr.forEach(item => result *= item);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let result = 1;
+	for (let i = 0; i < power; i++) {
+    result *= num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let factorial = 1;
+  for (let i = 1; i <= x; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
